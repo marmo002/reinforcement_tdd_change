64700ea71af6cb471a611810a8a40682134eeda4
marmo002 <marmo_02@hotmail.com>
2017-11-09 19:33:18 -0500

    Finish assignment

---
+++ changer.rb
@@ -0,0 +1,25 @@
+class Changer
+
+  def self.make_change(value)
+    coins = [1, 5, 10, 25]
+    change = []
+
+    ones = Array.new(value, 1)
+
+    if coins.include?(value)
+      change << value
+      return change
+    else
+      coins.reverse_each do |x|
+        while ones.count >= x
+            change << x
+            ones = ones.drop(x)
+        end
+      end
+    end
+
+    return change
+
+  end
+
+end
